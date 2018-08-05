8198e73002ba568d4f480342eeca89359832f91b
demulabNote2 <robot.engineer.koyo@gmail.com>
2018-08-05 23:52:40 +0900

    Shogo, Good Luck!

--- scripts/localization.py
+++ scripts/localization.py
@@ -9,12 +9,15 @@ import tf
 import actionlib
 import cv2
 import math
+import numpy as np
 from numpy.random import randn
+from gazebo_msgs.msg import ModelStates
 from visualization_msgs.msg import Marker
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist,Quaternion,PoseWithCovarianceStamped,PoseArray,Pose
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan,Imu
 from move_base_msgs.msg import MoveBaseAction,MoveBaseGoal
+from nav_msgs.msg import OccupancyGrid
 
 PARTICLE_NUM = 100
 
@@ -22,27 +25,76 @@ class Localization:
     def __init__(self):
         #publisher & subscriber
         self.vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop',Twist,queue_size=1)
-        #self.base_sub = rospy.Subscriber('/amcl_pose',PoseWithCovarianceStamped,self.BaseCB)
         self.particle_pub = rospy.Publisher('/e_particles',PoseArray,queue_size=1)
-        self.marker_pub = rospy.Publisher('/robot_pose',Marker,queue_size=1)
+        self.localized_pose_pub = rospy.Publisher('/localized_robot_pose',Marker,queue_size=1)
+        self.simulated_pose_pub = rospy.Publisher('/simulated_robot_pose',Marker,queue_size=1)
         self.laser_sub = rospy.Subscriber('/scan',LaserScan,self.LaserCB)
-        self.robot_pose_sub= rospy.Subscriber('/gazebo/model_states',LaserScan,self.visualizeSimulatedRobotPoseCB)
-        #for emergency stop
+        self.robot_pose_sub= rospy.Subscriber('/gazebo/model_states',ModelStates,self.visualizeSimulatedRobotPoseCB)
+        self.map_sub = rospy.Subscriber('/map',OccupancyGrid,self.mapCB)
+        self.imu_sub = rospy.Subscriber('/imu',Imu,self.BaseCB)
+        #particles
         self.particles = []
         self.initParticle()
+        #map
+        self.mymap = []
+        #for odometry
+        self.vel_x = 0.0
+        self.vel_y = 0.0
+        self.dist_x = 0.0
+        self.dist_y = 0.0
+        self.angle_z = 0.0
+        self.time = time.time()
+        #lidar
+        #self.lidar_dist = np.array()
 
 #---------------------------CallBack
-    def BaseCB(self,pose):
-        self.robot_pose_x = pose.pose.pose.position.x
-        self.robot_pose_y = pose.pose.pose.position.y
-        if self.emergency_stop_flg == False:
-            print 'remember now pose'
-            self.before_stop_pose = pose
-            
+    def BaseCB(self,msg):
+        time_diff = time.time() - self.time
+        self.time = time.time()
+
+        self.vel_x += msg.linear_acceleration.x * time_diff
+        self.vel_y += msg.linear_acceleration.y * time_diff
+        self.dist_x += self.vel_x * time_diff
+        self.dist_y += self.vel_y * time_diff
+        self.angle_z += msg.angular_velocity.z * time_diff
+        #print self.vel_x, self.vel_y, self.dist_x, self.dist_y
+
     def LaserCB(self,laser_scan):
-        #self.centor_laser_dist = laser_scan.ranges[405]
-        #self.laser_update_count += 1#use for detecting emergency stop
-        a = 0
+        self.lidar_dist = np.array(laser_scan.ranges)
+        print self.lidar_dist.shape
+
+    def mapCB(self,msg):
+        PIXEL_UNIT = 20#２０ピクセル１メートル
+        width = int(msg.info.width)
+        height = int(msg.info.height)
+        mymap = np.array(msg.data)
+        mymap = np.reshape(mymap,(width,height))
+        #print msg.info.origin
+
+    def visualizeSimulatedRobotPoseCB(self,model_state):
+        marker_data = Marker()
+        marker_data.header.frame_id = "map"
+        marker_data.header.stamp = rospy.Time.now()
+        marker_data.ns = "simulated_robot_pose"
+        marker_data.id = 1
+        marker_data.action = Marker.ADD
+        marker_data.pose.position.x = model_state.pose[2].position.x
+        marker_data.pose.position.y = model_state.pose[2].position.y
+        marker_data.pose.position.z = 0
+        marker_data.pose.orientation.x = model_state.pose[2].orientation.x
+        marker_data.pose.orientation.y = model_state.pose[2].orientation.y
+        marker_data.pose.orientation.z = model_state.pose[2].orientation.z
+        marker_data.pose.orientation.w = model_state.pose[2].orientation.w
+        marker_data.color.r = 0.0
+        marker_data.color.g = 0.0
+        marker_data.color.b = 1.0
+        marker_data.color.a = 1.0
+        marker_data.scale.x = 0.3
+        marker_data.scale.y = 0.1
+        marker_data.scale.z = 0.1
+        marker_data.lifetime = rospy.Duration()
+        marker_data.type = 0
+        self.simulated_pose_pub.publish(marker_data)
 
 #--------------------------
     def initParticle(self):
@@ -101,41 +153,26 @@ class Localization:
         marker_data.scale.z = 0.1
         marker_data.lifetime = rospy.Duration()
         marker_data.type = 0
-        print 114514
-        self.marker_pub.publish(marker_data)
+        self.localized_pose_pub.publish(marker_data)
 
-    def visualizeSimulatedRobotPoseCB(self,model_state):
-        marker_data = Marker()
-        marker_data.header.frame_id = "map"
-        marker_data.header.stamp = rospy.Time.now()
-        marker_data.ns = "simulated_robot_pose"
-        marker_data.id = 1
-        marker_data.action = Marker.ADD
-        marker_data.pose.position.x = model_state.pose[2].position.x
-        marker_data.pose.position.y = model_state.pose[2].position.y
-        marker_data.pose.position.z = 0
-        marker_data.pose.orientation.x = model_state.pose[2].orientation.x
-        marker_data.pose.orientation.y = model_state.pose[2].orientation.y
-        marker_data.pose.orientation.z = model_state.pose[2].orientation.z
-        marker_data.pose.orientation.w = model_state.pose[2].orientation.w
-        marker_data.color.r = 0.0
-        marker_data.color.g = 0.0
-        marker_data.color.b = 1.0
-        marker_data.color.a = 1.0
-        marker_data.scale.x = 0.3
-        marker_data.scale.y = 0.1
-        marker_data.scale.z = 0.1
-        marker_data.lifetime = rospy.Duration()
-        marker_data.type = 0
-        self.marker_pub.publish(marker_data)
         
+    def updateParticle(self):
+        rn = randn(2*PARTICLE_NUM)
+        for i in range(0,PARTICLE_NUM):
+            self.particles[i]["x"] += self.dist_y# + rn[i*2]
+            self.particles[i]["y"] -= self.dist_x# + rn[i*2+1]
+            self.particles[i]["pitch"] += self.angle_z
+        self.dist_x = 0.0
+        self.dist_y = 0.0
+        self.angle_z = 0.0
+
     def Localization(self):
-        print 111
-        mymap = cv2.imread("/home/demulab/map/mymap.pgm",0)
-        cv2.imshow("aaa",mymap)
-        cv2.waitKey(1)
+        #mymap = cv2.imread("/home/demulab/map/mymap.pgm",0)
+        #cv2.imshow("aaa",mymap)
+        #cv2.waitKey(1)
         self.visualizeParticle()
         self.visualizeLocalizedRobotPose()
+        self.updateParticle()
 
 if __name__ == '__main__':
     rospy.init_node('localization')
